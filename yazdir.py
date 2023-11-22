12a65cd27e8c8ff882656b1f7e7763c23c1fd74b
ilaydagunaydin <79969156+ilayda-gunaydin@users.noreply.github.com>
2023-11-22 22:25:49 +0300

    Update yazdir.py

--- yazdir.py
+++ yazdir.py
@@ -1,22 +1,19 @@
 import os
-from PyPDF2 import PdfReader, PdfWriter
+from PyPDF2 import PdfReader
 
-def list_pdf_titles(folder_path, output_file):
-    output_pdf = PdfWriter()
-
-    for filename in os.listdir(folder_path):
-        if filename.endswith('.pdf'):
-            file_path = os.path.join(folder_path, filename)
-            pdf = PdfReader(file_path)
-            title = pdf.metadata.title
-
-            output_pdf.add_page(pdf.pages[0])
-            output_pdf.add_metadata({'/Title': str(title)})
-
-    with open(output_file, 'wb') as f:
-        output_pdf.write(f)
+def write_pdf_titles_to_txt(folder_path, output_file):
+    with open(output_file, 'w', encoding='utf-8') as file:
+        for filename in os.listdir(folder_path):
+            if filename.endswith('.pdf'):
+                file_path = os.path.join(folder_path, filename)
+                with open(file_path, 'rb') as pdf_file:
+                    pdf = PdfReader(pdf_file)
+                    title = pdf.metadata.title
+                    if title:
+                        file.write(f'{filename}: {title}\n')
+                    else:
+                        file.write(f'{filename}: No title\n')
 
 folder_path = '/Users/ilayda/Desktop/PDF'
-output_file = '/Users/ilayda/Desktop/basliklar.pdf'
-
-list_pdf_titles(folder_path, output_file)
+output_file = 'pdf_titles.txt'
+write_pdf_titles_to_txt(folder_path, output_file)
